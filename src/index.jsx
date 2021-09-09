6009afb1f2329ab79fc0a0b00999da971457466a
Connor Davis <19422@kloofhigh.co.za>
2021-09-09 09:07:40 +0200

    attempt to make it a pwa

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,8 @@ import { render } from "solid-js/web";
 import "./index.css";
 import App from "./App";
 
+import * as serviceWorker from './serviceWorker';
+
 render(App, document.getElementById("root"));
+
+serviceWorker.register();
